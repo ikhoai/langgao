f5414a78fc5ee85e78970208bc263746c842db17
Minh Dinh <minh.dinh@smartbox.com>
2025-02-03 16:44:39 +0700

    update testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,22 +15,22 @@ import ngocAnh from '../images/ngoc-anh.JPG';
 const testimonials = [
   {
     name: 'Chị Ngọc Anh',
-    avatar: '../src/images/ngoc-anh.JPG',
+    avatar: '/images/ngoc-anh.JPG',
     testimonial: 'Mình luôn ưu tiên những sản phẩm sạch, hữu cơ vì cảm thấy yên tâm hơn khi dùng cho da. Làn da mình thuộc dạng thường, không quá nhạy cảm nhưng mình vẫn thích chăm sóc bằng những gì tự nhiên nhất. Lúc thử sản phẩm này, điều đầu tiên mình thích là cảm giác rất nhẹ nhàng, không hề có mùi hóa chất khó chịu như một số loại khác. Dùng một thời gian, da mềm mại hơn hẳn, nhìn khỏe khoắn và tươi tắn hơn. Mình thấy đây là lựa chọn lý tưởng cho ai yêu thích các sản phẩm lành tính như mình.',
   },
   {
     name: 'Hồng Nhung',
-    avatar: '../src/images/hong-nhung.jpg',
+    avatar: '/images/hong-nhung.jpg',
     testimonial: 'Mình mới làm mẹ lần đầu nên cái gì cũng lo lắng, đặc biệt là da của con. Mùa đông vừa rồi, hai má bé cứ khô nẻ, đỏ ửng lên, nhìn mà thương lắm. Lúc đó mình thật sự hoang mang vì không biết nên chọn loại nào. May mắn tìm được dòng kèm này, đọc kỹ thấy thành phần toàn tự nhiên nên mới dám dùng. Bôi thử sau một ngày thấy da con mềm hơn, đỏ rát cũng giảm hẳn. Thậm chí lỡ miệng con tiếp xúc với kem mình cũng không lo, vì biết là lành tính. Đúng là cứu tinh cho mẹ bỉm như mình.',
   },
   {
     name: 'Chị Nụ',
-    avatar: '../src/images/anh-tho.jpg',
+    avatar: '/images/anh-tho.jpg',
     testimonial: 'Da mình thuộc dạng siêu khô. Nhất là vào mùa đông còn có cảm giác ngừa râm ran khá là khó chịu. Lần đầu dùng mình thấy cảm giác ngứa dịu đi hẳn, sáng ngày mai sau khi rửa mặt trên bề mặt da vẫn có cảm giác ẩm mịn, sờ mướt tay. Sau khoảng 3-4 ngày sử dụng thì quan sát thấy da có sáng hơn, vết thâm mụn mờ đi rất rõ.',
   },
   {
     name: 'Anh Dũng',
-    avatar: '../src/images/anh-dung.jpg',
+    avatar: '/images/anh-dung.jpg',
     testimonial: 'Anh làm cơ khí, da tay thường xuyên khô ráp, nứt nẻ do tiếp xúc với nhiệt và dầu nhớt. Có nhiều hôm, cả bàn tay đau nhức vì da bị căng và bong tróc. Ban đầu anh không để ý chuyện dưỡng da vì nghĩ đó là việc của phụ nữ, nhưng vợ đưa cho loại kem này bảo thử. Dùng vài ngày thấy dịu hẳn, vết nứt lành nhanh. Anh thích nhất là hộp nhỏ gọn, dễ bỏ túi quần, tiện mang theo bên người để khi nào thấy cần là dùng ngay.',
   },
 ];
